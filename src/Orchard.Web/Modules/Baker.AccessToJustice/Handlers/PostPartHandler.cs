3a1ab5456563a3640f40e05235110dd4f1eedae6
eccsolutions <paul.davis@ecc-solutions.com>
2014-03-19 16:37:37 -0400

    Ran Code Cleanup

--- src/Orchard.Web/Modules/Baker.AccessToJustice/Handlers/PostPartHandler.cs
+++ src/Orchard.Web/Modules/Baker.AccessToJustice/Handlers/PostPartHandler.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Baker.AccessToJustice.Models;
+﻿using Baker.AccessToJustice.Models;
 using Baker.AccessToJustice.Services;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Handlers;
 using Orchard.Core.Common.Models;
 using Orchard.Data;
 
-namespace Baker.AccessToJustice.Handlers
-{
-    public class PostPartHandler : ContentHandler
-    {
-        private readonly IThreadService _threadService;
+namespace Baker.AccessToJustice.Handlers {
+    public class PostPartHandler : ContentHandler {
         private readonly IPostService _postService;
+        private readonly IThreadService _threadService;
 
-        public PostPartHandler(IRepository<PostPartRecord> repository, IThreadService threadService, IPostService postService) {
+        public PostPartHandler(IRepository<PostPartRecord> repository, IThreadService threadService,
+                               IPostService postService) {
             _threadService = threadService;
             _postService = postService;
             Filters.Add(StorageFilter.For(repository));
